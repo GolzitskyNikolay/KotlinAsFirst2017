e88c2b1e6b3a933b03420b420287ef1c73d7dccd
1 <Golzitskynikolay@gmail.com>
2017-09-21 21:16:26 +0300

    Lesson 1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -94,7 +94,8 @@ fun thirdDigit(number: Int): Int = (number/100)%10
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = abs((hoursArrive*60+minutesArrive)-(hoursDepart*60+minutesDepart))
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+        abs((hoursArrive*60+minutesArrive)-(hoursDepart*60+minutesDepart))
 
 /**
  * Простая
@@ -103,7 +104,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = initial*(1.0+percent/100.0)*(1.0+percent/100.0)*(1.0+percent/100.0)
+fun accountInThreeYears(initial: Int, percent: Int): Double =
+        initial*(1.0+percent/100.0)*(1.0+percent/100.0)*(1.0+percent/100.0)
 
 /**
  * Простая
